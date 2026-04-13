42a8f5ac3f442a97aff17449b34a00082c13f3b9
natalia-panagi <natalia.panagi@aol.com>
2026-04-13 21:32:01 +0300

    Added basic Js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+function generatePoem(event) {
+    event.preventDefault();
+   new Typewriter("#poem",{
+    strings: "La tombe dit a la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+
+   })
+
+
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
+
