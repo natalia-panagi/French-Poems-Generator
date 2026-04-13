24fcfebef746350e4f01a463a23f544046130849
natalia-panagi <natalia.panagi@aol.com>
2026-04-13 22:44:13 +0300

    Added Api Integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 
+console.log(document.querySelector("#user-instructions"));
+
+
+function displayPoem(response){
+    new Typewriter("#poem", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+
+}
+
+
 function generatePoem(event) {
     event.preventDefault();
-   new Typewriter("#poem",{
-    strings: "La tombe dit a la rose",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
+let instructionsInput = document.querySelector("#user-instructions");
+let key = "fb6a7o33d928a9e256t114bb04022298";
+let prompt =`Generate a French poem about ${instructionsInput.value}`;
+let context = "You are a French romantic poet. Write a 4-line poem in basic HTML, separating the lines with a br/. Don't include any other symbols, quote marks, html tag, only the poem text. Sign the poem with `SheCodes Ai` inside a <strong> element";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-   })
+axios.get(apiUrl).then(displayPoem);
+
+   
 
 
 }
